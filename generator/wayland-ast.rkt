8b6b3fe5597b91eb4a85af28da3c86f4dfe7961f
Anthony Carrico <acarrico@memebeam.org>
2016-11-18 14:11:39 -0500

    Query for the interface of new protocol objects
    In the protocol, new_id arguments send new objects. Query the ast for
    the interfaces of these objects.

--- generator/wayland-ast.rkt
+++ generator/wayland-ast.rkt
@@ -3,12 +3,18 @@
 (provide (struct-out About)
          (struct-out Protocol)
          (struct-out Interface) Interface-name
+         interface-new-interfaces
          (struct-out Message) Message-name
+         message-new-interface
          (struct-out Request)
          (struct-out Event)
          (struct-out Enum)
          (struct-out Arg) Arg-name
+         arg-new-id?
+         arg-new-interface
          (struct-out Entry))
+
+(require racket/list)
 
 (struct About (what name summary description) #:transparent)
 
@@ -17,8 +23,25 @@
 (struct Interface (about version requests events enums) #:transparent)
 (define (Interface-name i) (About-name (Interface-about i)))
 
+;; The names of the interfaces of the new objects sent by this
+;; interface.
+;; (: interface-new-interfaces (-> Interface (Listof String)))
+(define (interface-new-interfaces i)
+  (remove-duplicates
+   (for*/fold ((result '()))
+              ((m (append (map Request-message (Interface-requests i))
+                          (map Event-message (Interface-events i)))))
+     (cond ((message-new-interface m) => (lambda (i) (cons i result)))
+           (else result)))))
+
 (struct Message (about destructor? since args) #:transparent)
 (define (Message-name m) (About-name (Message-about m)))
+
+;; If the Message sends a new object, which interface?
+;; (: message-new-interface (-> Message (Option String)))
+(define (message-new-interface m)
+  (for/or ((arg (Message-args m)))
+    (arg-new-interface arg)))
 
 (struct Request (message) #:transparent)
 
@@ -28,5 +51,14 @@
 
 (struct Arg (about type summary interface-name allow-null) #:transparent)
 (define (Arg-name a) (About-name (Arg-about a)))
+
+;; Does the Arg send a new object?
+;; (: arg-new-id? (-> Arg bool))
+(define (arg-new-id? a) (string=? (Arg-type a) "new_id"))
+
+;; If the Arg sends a new object, which interface?
+;; (: arg-new-interface (-> Arg (Option String))
+(define (arg-new-interface a)
+  (if (arg-new-id? a) (Arg-interface-name a) #f))
 
 (struct Entry (about value summary since) #:transparent)
